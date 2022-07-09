0b1c0ccdfcfe6f8c9aebe943b5530819f1ea6c1b
Rogerio Pessanha <rogerioo_pessanha@yahoo.com.br>
2022-07-09 00:08:08 -0300

    att

---
+++ exercicios/ex08/ex8.js
@@ -0,0 +1,7 @@
+var vel = 59
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60){
+    console.log(`Você utrapassou a velocidade permitida. Multado!`)
+}
+
+console.log(`Dirija sempre usando o cinto de segurança`)
---
+++ exercicios/ex09/ex9.js
@@ -0,0 +1,7 @@
+var pais = 'Brasil'
+console.log(`Vivendo no ${pais}`)
+if (pais == 'Brasil'){
+    console.log('Você é Brasileiro')
+} else{
+    console.log('Você é Estrangeira')
+}
