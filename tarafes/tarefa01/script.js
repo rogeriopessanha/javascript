7bdae78cb54bbb50536d63ed27b7bf1f2cf555eb
Rogerio Pessanha <rogerioo_pessanha@yahoo.com.br>
2022-07-13 22:33:40 -0300

    att

---
+++ tarafes/tarefa01/script.js
@@ -0,0 +1,22 @@
+function carregar () {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    //var hora = data.getHours()
+    var hora = 16
+   
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if(hora >= 0 && hora < 12){
+        //BOM DIA
+        img.src = 'imagens/manhaa.png'
+        document.body.style.background = '#E6E2C3'
+    }else if (hora >= 12 && hora <= 18) {
+        //BOA TARDE
+        img.src = 'imagens/tardee.png'
+        document.body.style.background = '#F5967B'  
+    } else {
+        //BOA NOITE
+        img.src = 'imagens/noitee.png'
+        document.body.style.background = '#445F70'
+    }
+}
