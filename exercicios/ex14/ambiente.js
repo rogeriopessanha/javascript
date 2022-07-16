e94e44b4a37e2aef6a76ce9759ad9d8e7f22b44b
Rogerio Pessanha <rogerioo_pessanha@yahoo.com.br>
2022-07-16 01:44:55 -0300

    att

---
+++ exercicios/ex14/ambiente.js
@@ -0,0 +1,26 @@
+/*estrutura do(faça)...
+            while(enquanto) */
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while(c <= 5)
+
+
+
+/* estrutura while(enquanto)
+
+var c = 1
+while(c <= 5){
+    console.log('tudo bem?')
+    c++
+}
+*/
+
+
+/*pior opção para repetições
+console.log(tudo bem?)
+console.log(tudo bem?)
+console.log(tudo bem?)
+console.log(tudo bem?)   
+*/
---
+++ exercicios/ex15/ex001.js
@@ -0,0 +1,5 @@
+console.log('Vai começar a contagem...')
+for(var c = 1;c <= 5;c++){
+    console.log(c)
+}
+console.log('Fim.')
