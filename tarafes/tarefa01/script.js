366fbc1cb7ec4886e64ab93f9b824c858cc54cfc
Rogerio Pessanha <rogerioo_pessanha@yahoo.com.br>
2022-08-02 10:51:47 -0300

    att

--- tarafes/tarefa01/script.js
+++ tarafes/tarefa01/script.js
@@ -1,46 +1,45 @@
 function carregar () {
-    var msg = window.document.getElementById('msg')
-    var img = window.document.getElementById('imagem')
-    var recado = window.document.getElementById('recado')
+    let msg = window.document.getElementById('msg')
+    let img = window.document.getElementById('imagem')
+    let recado = window.document.getElementById('recado')
 
     const tempo = setInterval(() =>{
         let datahoje = new Date();
         let horas = datahoje.getHours();
         let minutos = datahoje.getMinutes();
         let segundos = datahoje.getSeconds();
-        //msg.innerHTML = `Que horas são: ${horas}:${minutos}:${segundos}`
-    
+        
         if(horas < 10) horas = '0' + horas;
         if(minutos < 10) minutos = '0' + minutos;
         if(segundos < 10) segundos = '0' + segundos;
-
         document.getElementById('horas').innerHTML = horas
         document.getElementById('minutos').innerHTML = minutos
         document.getElementById('segundos').innerHTML = segundos
+
+        if(horas >= 0 && horas < 6){
+            img.src = 'imagens/madrugadaa.png'
+            document.body.style.background = '#D2ADD7'
+            //recado.innerHTML += `Tenha uma boa madrugada.`
+            //MADRUGADA
+        }else if(horas > 6 && horas < 12){
+            //BOM DIA
+            img.src = 'imagens/manhaa.png'
+            document.body.style.background = '#91EE93'
+            //recado.innerHTML += `Tenha um bom dia.`
+        }else if (horas >= 12 && horas <= 18) {
+            //BOA TARDE
+            img.src = 'imagens/tardee.png'
+            document.body.style.background = '#F5967B' 
+            //recado.innerHTML += `Tenha uma boa tarde.` 
+        } else {
+            //BOA NOITE
+            img.src = 'imagens/noitee.png'
+            document.body.style.background = '#445F70'
+            //recado.innerHTML += `Tenha uma boa noite.`
+        }   
+
+
     })
 
-    
-    
-    if(horas >= 0 && horas < 6){
-        img.src = 'imagens/madrugadaa.png'
-        document.body.style.background = '#D2ADD7'
-        recado.innerHTML += `Tenha uma boa madrugada.`
-        //MADRUGADA
-    }else if(horas > 6 && horas < 12){
-        //BOM DIA
-        img.src = 'imagens/manhaa.png'
-        document.body.style.background = '#91EE93'
-        recado.innerHTML += `Tenha um bom dia.`
-    }else if (horas >= 12 && horas <= 18) {
-        //BOA TARDE
-        img.src = 'imagens/tardee.png'
-        document.body.style.background = '#F5967B' 
-        recado.innerHTML += `Tenha uma boa tarde.` 
-    } else {
-        //BOA NOITE
-        img.src = 'imagens/noitee.png'
-        document.body.style.background = '#445F70'
-        recado.innerHTML += `Tenha uma boa noite.`
-    }   
 }
 
